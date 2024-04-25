96bd60856831ee7af1e48b9c220a10bd92eb340d
Deepshikha-7 <152361526+Deepshikha-7@users.noreply.github.com>
2024-04-25 22:02:38 +0530

    Product Management System
    A product Management System to track the details about the products.

---
+++ Product_Management/src/main/java/com/service/ProductService.java
@@ -0,0 +1,16 @@
+package com.service;
+
+import java.util.List;
+
+import com.model.Product;
+
+public interface ProductService {
+
+	public Product saveProduct(Product product);
+	public List<Product> getAllProduct();
+	public Product getProductById(Integer id);
+	public String deleteProduct(Integer id);
+	
+	public Product editProduct(Product product,Integer id);
+	
+}
